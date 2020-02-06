5aca5613607a1658330d2fc3c6a6ccafbf2aac07
iAndreeaLavinia <andreea.ionescu@orange.com>
2020-02-06 14:31:48 +0200

    Lesson 4 Switch between List and Grid screens

--- ArtGallery/Gallery/GalleriesViewController.swift
+++ ArtGallery/Gallery/GalleriesViewController.swift
@@ -14,15 +14,55 @@ class GalleriesViewController: UIViewController {
     @IBOutlet weak var screenTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var screenContainerView: UIView!
     
+    
+    private lazy var galleriesTableViewController: GalleriesTableViewController = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "GalleriesTableViewController") as! GalleriesTableViewController
+        // Add View Controller as Child View Controller
+        self.add(asChildViewController: viewController)
+
+        return viewController
+    }()
+
+    private lazy var galleriesCollectionViewController: GalleriesCollectionViewController = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "GalleriesCollectionViewController") as! GalleriesCollectionViewController
+
+        // Add View Controller as Child View Controller
+        self.add(asChildViewController: viewController)
+
+        return viewController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func screenTypeChanged(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 1 {
-            screenContainerView.isHidden = true
+            add(asChildViewController: galleriesCollectionViewController)
         } else {
-            screenContainerView.isHidden = false
+            add(asChildViewController: galleriesTableViewController)
         }
+    }
+    
+    private func add(asChildViewController viewController: UIViewController) {
+        // Add Child View Controller
+        addChild(viewController)
+
+        // Add Child View as Subview
+        screenContainerView.addSubview(viewController.view)
+
+        // Configure Child View
+        viewController.view.frame = screenContainerView.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        // Notify Child View Controller
+        viewController.didMove(toParent: self)
     }
 }
